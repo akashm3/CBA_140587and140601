74b489821e41f6e5374d9858aa18c96811ce9632
akashm3 <50397122+akashm3@users.noreply.github.com>
2019-05-08 15:13:02 +0530

    Update file1.c

--- file1.c
+++ file1.c
@@ -4,14 +4,12 @@ int main()
     int i, j, rows;
 
     printf("Enter number of rows: ");
-   // scanf("%d",&rows);
-
     rows=4;
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
         {
-            printf("* ");
+            printf("# ");
         }
         printf("\n");
     }
